f934abbaf800fc1c2cdeb08d432ad1486811ec27
Exter-dg <prkandmap23@rocketmail.com>
2021-09-20 08:44:47 +0530

    Add Activities for created layouts

---
+++ app/src/main/java/com/example/techinquiri/GetStarted1Activity.java
@@ -0,0 +1,13 @@
+package com.example.techinquiri;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetStarted1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_started1);
+    }
+}
---
+++ app/src/main/java/com/example/techinquiri/StoryQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.techinquiri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story_questions);
+    }
+}
