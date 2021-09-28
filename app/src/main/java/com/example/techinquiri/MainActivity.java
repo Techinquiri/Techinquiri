5123278576da73e90ef09108c628e2da1c438cf2
khushi2912 <foodaholicclasher@gmail.com>
2021-09-28 21:02:50 +0530

    Add static stories

--- app/src/main/java/com/example/techinquiri/MainActivity.java
+++ app/src/main/java/com/example/techinquiri/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.techinquiri.data.dbHandler;
 
 public class MainActivity extends AppCompatActivity {
 
     Button skip;
+    dbHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +26,19 @@ public class MainActivity extends AppCompatActivity {
         {
             public void onClick(View view)
             {
+                Boolean checkInsert1 = db.insert("Yaksh & Pandavas");
+                Boolean checkInsert2 = db.insert("Squirrel's Effort for RamSetu");
+                Boolean checkInsert3 = db.insert("Pandora's Box");
+                if(checkInsert1==false | checkInsert2==false | checkInsert3==false)
+                {
+                    Toast.makeText(MainActivity.this, "Error Occured", Toast.LENGTH_SHORT).show();
+                }
                 Intent intent = new Intent(MainActivity.this, StoriesHomeActivity.class);
                 startActivity(intent);
             }
         });
+
+        db = new dbHandler(this);
+
     }
 }
